3a4dfa1ca11120fae73aa5a823ea941e00d7903b
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-11-07 23:29:46 +0530

    Fixed the nav bar position

--- static_dir/assets/js/main.js
+++ static_dir/assets/js/main.js
@@ -1,43 +1,59 @@
 /**
-* Template Name: Folio - v2.2.1
-* Template URL: https://bootstrapmade.com/folio-bootstrap-portfolio-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function($) {
+ * Template Name: Folio - v2.2.1
+ * Template URL: https://bootstrapmade.com/folio-bootstrap-portfolio-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function ($) {
   "use strict";
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
-  var scrolltoOffset = $('#main-nav').outerHeight() - 1;
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        e.preventDefault();
+  var scrolltoOffset = $("#main-nav").outerHeight() - 1;
+  $(document).on(
+    "click",
+    ".nav-menu a, .mobile-nav a, .scrollto",
+    function (e) {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        if (target.length) {
+          e.preventDefault();
 
-        var scrollto = target.offset().top - scrolltoOffset;
+          var scrollto = target.offset().top - scrolltoOffset;
 
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
+          $("html, body").animate(
+            {
+              scrollTop: scrollto,
+            },
+            1500,
+            "easeInOutExpo"
+          );
 
-        if (window.matchMedia("(max-width:991px)").matches) {
-          $('.nav-menu').hide();
+          if (window.matchMedia("(max-width:991px)").matches) {
+            $(".nav-menu").hide();
+          }
+          return false;
         }
-        return false;
       }
     }
-  });
+  );
 
   // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function() {
+  $(document).ready(function () {
     if (window.location.hash) {
       var initial_nav = window.location.hash;
       if ($(initial_nav).length) {
         var scrollto = $(initial_nav).offset().top - scrolltoOffset;
-        $('html, body').animate({
-          scrollTop: scrollto
-        }, 1500, 'easeInOutExpo');
+        $("html, body").animate(
+          {
+            scrollTop: scrollto,
+          },
+          1500,
+          "easeInOutExpo"
+        );
       }
     }
   });
@@ -45,24 +61,24 @@
   // ========================================================================= //
   //  //NAVBAR SHOW - HIDE
   // ========================================================================= //
+  $("#main-nav").slideDown(700);
 
-  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (!$('.subpage-nav').length) {
-      if (scroll > 200) {
-        $("#main-nav").slideDown(700);
-      } else {
-        $("#main-nav").slideUp(700);
-      }
-    }
-  });
+  // $(window).scroll(function() {
+  //   var scroll = $(window).scrollTop();
+  //   if (!$('.subpage-nav').length) {
+  //     if (scroll > 200) {
+  //     } else {
+  //       $("#main-nav").slideUp(700);
+  //     }
+  //   }
+  // });
 
   // ========================================================================= //
   //  // RESPONSIVE MENU
   // ========================================================================= //
 
-  $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
+  $(".responsive").on("click", function (e) {
+    $(".nav-menu").slideToggle();
   });
 
   // ========================================================================= //
@@ -71,10 +87,10 @@
 
   var typed = $(".typed");
 
-  $(function() {
-    var strings = $('.typed-items').text();
-    strings = $('.typed-items').data('typed-person') + ',' + strings;
-    strings = strings.split(',');
+  $(function () {
+    var strings = $(".typed-items").text();
+    strings = $(".typed-items").data("typed-person") + "," + strings;
+    strings = strings.split(",");
 
     typed.typed({
       strings: strings,
@@ -87,7 +103,7 @@
   //  Owl Carousel Services
   // ========================================================================= //
 
-  $('.services-carousel').owlCarousel({
+  $(".services-carousel").owlCarousel({
     autoplay: true,
     loop: true,
     margin: 20,
@@ -96,15 +112,15 @@
     responsiveClass: true,
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       768: {
-        items: 2
+        items: 2,
       },
       900: {
-        items: 4
-      }
-    }
+        items: 4,
+      },
+    },
   });
 
   // Testimonials carousel (uses the Owl Carousel library)
@@ -114,38 +130,38 @@
     loop: true,
     responsive: {
       0: {
-        items: 1
+        items: 1,
       },
       768: {
-        items: 2
+        items: 2,
       },
       900: {
-        items: 3
-      }
-    }
+        items: 3,
+      },
+    },
   });
   // ========================================================================= //
   //  Porfolio isotope and filter
   // ========================================================================= //
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
+  $(window).on("load", function () {
+    var portfolioIsotope = $(".portfolio-container").isotope({
+      itemSelector: ".portfolio-item",
+      layoutMode: "fitRows",
     });
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
+    $("#portfolio-flters li").on("click", function () {
+      $("#portfolio-flters li").removeClass("filter-active");
+      $(this).addClass("filter-active");
 
       portfolioIsotope.isotope({
-        filter: $(this).data('filter')
+        filter: $(this).data("filter"),
       });
     });
   });
 
   // Initiate venobox (lightbox feature used in portofilo)
-  $(document).ready(function() {
-    $('.venobox').venobox();
+  $(document).ready(function () {
+    $(".venobox").venobox();
   });
 
   // Portfolio details carousel
@@ -153,7 +169,6 @@
     autoplay: true,
     dots: true,
     loop: true,
-    items: 1
+    items: 1,
   });
-
 })(jQuery);
