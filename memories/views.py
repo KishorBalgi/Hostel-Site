febbf264e698e7583c02bb768a8a26f2a88cc8cd
RajathPrabhu221 <bangu2001@gmail.com>
2021-11-07 11:29:40 +0530

    Folder renamed

---
+++ memories/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .forms import WishesForm
+from .models import Wishes
+
+def wishes(request):
+    wishes = Wishes.objects.all().order_by('-timestamp')
+    if request.POST:
+        form = WishesForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('home')
+    else:
+        form = WishesForm()
+    
+    context = {
+        'form': form,
+        'wishes': wishes
+    }
+    return render(request, 'blog-single.html', context=context)
